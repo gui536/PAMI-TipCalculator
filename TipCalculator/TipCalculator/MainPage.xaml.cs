d5d853b8ad76da94515fb2961ade24ea1fd4a545
Guilherme Santos <guilherme.santos536@etec.sp.gov.br>
2024-03-11 22:46:48 -0300

    feat: Adicionei o calcular gorjeta

---
+++ TipCalculator/TipCalculator/MainPage.xaml.cs
@@ -0,0 +1,64 @@
+﻿namespace TipCalculator
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void SliderTipPercent_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            LabelPercentageValue.Text = Math.Round(SliderTipPercent.Value).ToString();
+        }
+
+        private void OnButton15PercentClicked(object sender, EventArgs e)
+        {
+            SliderTipPercent.Value = 15;
+        }
+
+        private void OnButton20PercentClicked(object sender, EventArgs e)
+        {
+            SliderTipPercent.Value = 20;
+        }
+
+        private void OnButtonRoundDownClicked(object sender, EventArgs e)
+        {
+            // calcular a gorjeta, arredondando para baixo
+            double result = CalculateTip();
+            double roundedResult = Math.Floor(result);
+            double amount = Convert.ToDouble(ValueEntry.Text.ToString());
+            double totalValue = roundedResult + amount;
+            // exibir a gorjeta
+            LabelTipValue.Text = roundedResult.ToString();
+            LabelTotalValue.Text = totalValue.ToString();
+        }
+
+        private void OnButtonRoundUpClicked(object sender, EventArgs e)
+        {
+            // calcular a gorjeta, arredondando para cima
+            double result = CalculateTip();
+            double roundedResult = Math.Ceiling(result);
+            double amount = Convert.ToDouble(ValueEntry.Text.ToString());
+            double totalValue = roundedResult + amount;
+            // exibir a gorjeta
+            LabelTipValue.Text = roundedResult.ToString();
+            LabelTotalValue.Text = totalValue.ToString();
+        }
+
+        private double CalculateTip()
+        {
+            
+            double amount = Convert.ToDouble(ValueEntry.Text.ToString());
+            double percent = SliderTipPercent.Value;
+
+            // calculo da gorjeta
+            double result = amount * (percent / 100);
+            return result;
+        }
+
+    }
+
+
+}
